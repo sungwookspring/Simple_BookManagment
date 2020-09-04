b824ebd11d25dd237e2444962cf2df3bb62919e3
choisungwook <kgg1959@naver.com>
2020-09-04 20:39:49 +0900

    프로젝트 생성

---
+++ src/main/java/com/checkbox/checkbox/CheckboxApplication.java
@@ -0,0 +1,13 @@
+package com.checkbox.checkbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkbox/checkbox/CheckboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkbox.checkbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
