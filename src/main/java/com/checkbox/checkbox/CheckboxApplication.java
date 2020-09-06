25750152bae45a565ac4297e9e5149ae1ad4a38d
choisungwook <kgg1959@naver.com>
2020-09-06 22:01:46 +0900

    Add insert sql for test

--- src/main/java/com/checkbox/checkbox/CheckboxApplication.java
+++ src/main/java/com/checkbox/checkbox/CheckboxApplication.java
@@ -1,13 +1,46 @@
 package com.checkbox.checkbox;
 
+import com.checkbox.checkbox.domain.Book;
+import com.checkbox.checkbox.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class CheckboxApplication {
+	private final BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckboxApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner Register() {
+		return (args) -> {
+			Book book1 = Book.builder()
+					.title("test1")
+					.author("author1")
+					.build();
+			Book book2 = Book.builder()
+					.title("test2")
+					.author("author2")
+					.build();
+			Book book3 = Book.builder()
+					.title("test3")
+					.author("author3")
+					.build();
+			Book book4 = Book.builder()
+					.title("test4")
+					.author("author5")
+					.build();
+
+			bookRepository.save(book1);
+			bookRepository.save(book2);
+			bookRepository.save(book3);
+			bookRepository.save(book4);
+		};
+	}
 }
