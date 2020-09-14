ccb6b671d144dd6aed54c1add55e16dc9095d64b
choisungwook <kgg1959@naver.com>
2020-09-14 19:31:30 +0900

    Change BookCategory 저장기능: Dto를 파라미터로 받아 생성(기존 파라미터: 엔티티)

--- src/main/java/com/checkbox/checkbox/service/BookCategoryService.java
+++ src/main/java/com/checkbox/checkbox/service/BookCategoryService.java
@@ -1,7 +1,12 @@
 package com.checkbox.checkbox.service;
 
+import com.checkbox.checkbox.domain.Book;
 import com.checkbox.checkbox.domain.BookCategory;
+import com.checkbox.checkbox.domain.Category;
+import com.checkbox.checkbox.domain.Dto.BookCategory.BookCategoryRequestAddDto;
 import com.checkbox.checkbox.repository.BookCategoryRepository;
+import com.checkbox.checkbox.repository.BookRepository;
+import com.checkbox.checkbox.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +18,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookCategoryService {
     private final BookCategoryRepository bookCategoryRepository;
+    private final BookRepository bookRepository;
+    private final CategoryRepository categoryRepository;
 
     @Transactional
-    public Long save(BookCategory bookCategory){
-        return bookCategoryRepository.save(bookCategory).getId();
+    public Long save(BookCategoryRequestAddDto requestAddDto){
+        Book findBook = bookRepository.findById(requestAddDto.getBook_id())
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않은 book_id")
+                );
+
+        Category findCategory = categoryRepository.findById(requestAddDto.getCategory_id())
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않은 category_id")
+                );
+
+
+        // 카테코리 연결
+        BookCategory new_bookcategory = BookCategory.builder()
+                .category(findCategory)
+                .build();
+
+        // 책 연결
+        findBook.setRelationWithBookCategory(new_bookcategory);
+
+        return bookCategoryRepository.save(new_bookcategory).getId();
     }
 
     public List<BookCategory> findAll(){
