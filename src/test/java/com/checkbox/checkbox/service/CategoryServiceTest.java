f364c01e39d308a6f71a73eeb7dc4eeefb11480e
choisungwook <kgg1959@naver.com>
2020-09-14 19:32:42 +0900

    Change BookCategory 테스트: 저장방법을 엔티티 -> Dto방법으로 변경

--- src/test/java/com/checkbox/checkbox/service/CategoryServiceTest.java
+++ src/test/java/com/checkbox/checkbox/service/CategoryServiceTest.java
@@ -4,6 +4,7 @@ import com.checkbox.checkbox.domain.Book;
 import com.checkbox.checkbox.domain.BookCategory;
 import com.checkbox.checkbox.domain.Category;
 import com.checkbox.checkbox.domain.Dto.Book.BookRequestAddDto;
+import com.checkbox.checkbox.domain.Dto.BookCategory.BookCategoryRequestAddDto;
 import com.checkbox.checkbox.domain.Dto.Category.CategoryRequestAddDto;
 import com.checkbox.checkbox.repository.BookCategoryRepository;
 import com.checkbox.checkbox.repository.BookRepository;
@@ -56,39 +57,73 @@ public class CategoryServiceTest {
     @Test
     @Transactional
     public void createBook_createCategory_AndDoRelation(){
+        // given
+
         // 책 생성
-        BookRequestAddDto bookdto = new BookRequestAddDto();
-        bookdto.setAuthor("테스트 작가");
-        bookdto.setTitle("테스트 책");
-        Long savedBookId = bookService.save(bookdto);
-        Book findBook = bookRepository.findById(savedBookId)
-                .orElseThrow(
-                        () -> new IllegalStateException("존재하지 않은 Book_id")
-                );
+        Book savedBook =  createbook_checkId("테스트 책", "테스트 작가");
 
         // 카테고리 생성
-        CategoryRequestAddDto categorydto = new CategoryRequestAddDto();
-        categorydto.setName("교양");
-        Long saveCategoryId = categoryService.save(categorydto);
-
-        // 책<>카테고리
-        Category savedCategory = categoryRepository.findById(saveCategoryId)
-                .orElseThrow(
-                        () -> new IllegalStateException("존재하지 않은 카테고리 ID")
-                );
+        Category savedCategory = createCategory_checkId("테스트 카테고리");
 
         // when
-        BookCategory bookCategory = BookCategory.builder()
-                .category(savedCategory)
-                .build();
+        BookCategoryRequestAddDto requestAddDto = new BookCategoryRequestAddDto();
+        requestAddDto.setBook_id(savedBook.getId());
+        requestAddDto.setCategory_id(savedCategory.getId());
 
-        Long savedBookCategoryId = bookCategoryService.save(bookCategory);
+        Long savedBookCategoryId = bookCategoryService.save(requestAddDto);
+
+        //then
         BookCategory findBookCategory = bookCategoryRepository.findById(savedBookCategoryId)
                 .orElseThrow(
                         () -> new IllegalStateException("존재하지 않은 savedBookCategoryId")
                 );
 
-        findBook.setRelationWithBookCategory(findBookCategory);
+
+        // Id 비교
+        Long findCategoryId = findBookCategory.getCategory().getId();
+        Long findBookId =  findBookCategory.getBook().getId();
+
+        Assertions.assertThat(findCategoryId).isEqualTo(savedCategory.getId());
+        Assertions.assertThat(findBookId).isEqualTo(savedBook.getId());
+
+        // 이름 비교
+        String findCategoryName = findBookCategory.getCategory().getName();
+        String findBookName =  findBookCategory.getBook().getTitle();
+
+        Assertions.assertThat(findCategoryName).isEqualTo(savedCategory.getName());
+        Assertions.assertThat(findBookName).isEqualTo(savedBook.getTitle());
+    }
+
+    public Book createbook_checkId(String title, String author){
+        BookRequestAddDto bookdto = new BookRequestAddDto();
+        bookdto.setAuthor("title");
+        bookdto.setTitle("author");
+        Long savedBookId = bookService.save(bookdto);
+
+        Book findBook =  bookRepository.findById(savedBookId)
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않은 book_id")
+                );
+
+        return findBook;
+    }
+
+    public Category createCategory_checkId(String name){
+        CategoryRequestAddDto requestAddDto = new CategoryRequestAddDto();
+        requestAddDto.setName(name);
+
+        //when
+        Long saveId = categoryService.save(requestAddDto);
+
+        //then
+        Category findCategory = categoryRepository.findById(saveId)
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않은 카테고리 ID")
+                );
+
+        Assertions.assertThat(findCategory.getName()).isEqualTo(requestAddDto.getName());
+
+        return findCategory;
     }
 
 }
