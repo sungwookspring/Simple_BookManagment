77f8e3614929e43ec3563825378de4e860585339
choisungwook <kgg1959@naver.com>
2020-09-14 13:46:43 +0900

    Add 카테고리 등록 Controller

--- src/main/java/com/checkbox/checkbox/controller/Category_Controller.java
+++ src/main/java/com/checkbox/checkbox/controller/Category_Controller.java
@@ -4,15 +4,19 @@ import com.checkbox.checkbox.domain.Category;
 import com.checkbox.checkbox.domain.Dto.Category.CategoryRequestAddDto;
 import com.checkbox.checkbox.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class Category_Controller {
     private final CategoryService categoryService;
 
@@ -33,5 +37,16 @@ public class Category_Controller {
         return "category/list";
     }
 
+    @GetMapping("/category/add")
+    public String saveForm(Model model){
+        model.addAttribute("requestAddDto", new CategoryRequestAddDto());
+        return "category/add";
+    }
 
+    @PostMapping("/category/add")
+    public String save(@ModelAttribute CategoryRequestAddDto requestAddDto){
+        Long saveId = categoryService.save(requestAddDto);
+        return "redirect:/category/list";
+    }
 }
+
