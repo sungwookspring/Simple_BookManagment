123dfc458c39a4e05b2a44a9234ebd62211ca712
choisungwook <kgg1959@naver.com>
2020-09-21 22:11:28 +0900

    Change CategoryServiceTest: 필요없는 코드 삭제

--- src/test/java/com/checkbox/checkbox/service/CategoryServiceTest.java
+++ src/test/java/com/checkbox/checkbox/service/CategoryServiceTest.java
@@ -17,8 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CategoryServiceTest {
@@ -117,7 +115,6 @@ public class CategoryServiceTest {
         Assertions.assertThat(findCategoryName).isEqualTo(savedCategory.getName());
         Assertions.assertThat(findBookName).isEqualTo(savedBook.getTitle());
     }
-
 
     private BookCategory createBookCategory(Long book_id, Long category_id){
         BookCategoryRequestAddDto requestAddDto = new BookCategoryRequestAddDto();
